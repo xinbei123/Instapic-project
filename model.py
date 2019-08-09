47e6ccc6ef9c3fd14fc567d9e2b89150d9a5d444
Aileen Liu <aileenliu184@gmail.com>
2019-08-09 01:55:43 -0700

    add comment boxes to a list of photos

---
+++ model.py
@@ -0,0 +1,124 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+##############################################################################
+# Model definitions
+
+class User(db.Model):
+    """Show user info"""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    username = db.Column(db.String(30), nullable=False, unique=True)
+    email = db.Column(db.String(64), nullable=True)
+    password = db.Column(db.String(64), nullable=True)
+    date_created = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+
+            return f"<User user_id={self.user_id} username={self.username}>"
+
+class Photo(db.Model):
+    """Show photo info"""
+
+    __tablename__ = "photos"
+
+    photo_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    photo_user_id = db.Column(db.Integer, nullable=False)
+    date_uploaded = db.Column(db.DateTime, nullable=False)
+    name = db.Column(db.String, nullable=True)
+    photo_url = db.Column(db.String, nullable=False)
+
+    hashtags = db.relationship("Hashtag", secondary="photohashtags",
+                                backref="photos")
+
+    
+    def __repr__(self):
+
+            return f"""<Photo photo_id={self.photo_id} 
+                       photo_user_id={self.photo_user_id}
+                       photo_url={self.photo_url}>"""
+
+
+class Comment(db.Model):
+    """Show comment about photo by user"""
+
+    __tablename__ = "comments"
+
+    comment_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    comment = db.Column(db.String, nullable=True)
+    photo_id = db.Column(db.Integer, db.ForeignKey('photos.photo_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+
+    users = db.relationship("User",
+                               backref=db.backref("comments",
+                                                  order_by=comment_id))
+    photos = db.relationship("Photo",
+                               backref=db.backref("photos",
+                                                  order_by=photo_id))
+
+    def __repr__(self):
+
+            return f"""<Comment comment_id={self.comment_id} 
+                       photo_id={self.photo_id}
+                       user_id={self.user_id} 
+                       >"""
+
+class Hashtag(db.Model):
+    """Show Hashtag info about photo"""
+
+    __tablename__ = "hashtags"
+
+    hashtag_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    hashtag = db.Column(db.String, nullable=True, unique=True)
+    
+    def __repr__(self):
+
+            return f"""<Hashtag hashtag_id={self.hashtag_id} 
+                       hashtag={self.hashtag}>"""
+
+class Photohashtag(db.Model):
+    """Association table between photo and hashtag"""
+
+    __tablename__ = "photohashtags"
+
+    photohashtag_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    photo_id = db.Column(db.Integer, 
+                         db.ForeignKey('photos.photo_id'), nullable=False)
+    hashtag_id = db.Column(db.Integer, 
+                           db.ForeignKey('hashtags.hashtag_id'), nullable=False)
+
+    def __repr__(self):
+
+            return f"""<Photohashtag photohashtag_id={self.photohashtag_id} 
+                       photo_id={self.photo_id}
+                       hashtag_id = {self.hashtag_id}>"""
+
+
+##############################################################################
+# Helper functions
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PstgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///instapic'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    from server import app
+    connect_to_db(app)
+    print("Connected to DB.")
+
+
+
+
--- server.py
+++ server.py
@@ -6,45 +6,76 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 from jinja2 import StrictUndefined
 
+from model import connect_to_db, db, User, Photo, Comment, Hashtag
+
 
 app = Flask(__name__)
+app.secret_key = "ABCdefGHL1234***$$$"
 
 app.jinja_env.undefined = StrictUndefined
 
 
-CAT_PIC = {
-    'auden': 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_960_720.jpg',
-    'rocket': 'https://cdn.pixabay.com/photo/2017/07/25/01/22/cat-2536662_960_720.jpg',
-    'muffin': 'https://cdn.pixabay.com/photo/2016/12/30/17/27/cat-1941089_960_720.jpg',
-    'fido': 'https://cdn.pixabay.com/photo/2016/03/27/07/31/pet-1282309_960_720.jpg'
-}
+# CAT_PIC = {
+#     'auden': 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_960_720.jpg',
+#     'rocket': 'https://cdn.pixabay.com/photo/2017/07/25/01/22/cat-2536662_960_720.jpg',
+#     'muffin': 'https://cdn.pixabay.com/photo/2016/12/30/17/27/cat-1941089_960_720.jpg',
+#     'fido': 'https://cdn.pixabay.com/photo/2016/03/27/07/31/pet-1282309_960_720.jpg'
+# }
 
 
 @app.route('/')
 def homepage():
 
-    return render_template('home.html', cats=CAT_PIC)
+    return render_template('homepage.html')
 
-@app.route('/about')
-def about():
+@app.route('/photos')
+def photo_list():
 
-    return render_template('about.html')
+    photos = Photo.query.all()
+    return render_template('photo_list.html', photos=photos)
 
+@app.route('/photos/<int:photo_id>', methods=['GET'])
+def pic_detail(photo_id):
 
+    photo = Photo.query.get(photo_id)
+    return render_template('photo_detail.html', photo=photo)
 
+@app.route('/photos/<int:photo_id>', methods=['POST'])
+def make_comment(photo_id):
 
+    comment = request.form.get('comment')
 
+    return render_template('photo_detail.html', comment=comment)
 
+@app.route('/register', methods=['GET'])
+def register_form():
 
+    return render_template('register_form.html')
 
+@app.route('/register', methods=['POST'])
+def register_process():
 
+    return redirect('/')
 
+@app.route('/login', methods=['GET'])
+def login_form():
+
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login_process():
+
+    return redirect('/')
+
+@app.route('/logout')
+def logout():
+
+    return redirect('/')
 
 
 if __name__ == "__main__":
-    
-    app.debug = True
 
+    app.debug = True
     app.jinja_env.auto_reload = app.debug
 
     connect_to_db(app)
